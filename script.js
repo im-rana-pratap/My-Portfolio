bd93923b325ef01ef27cbf64751476bda4016112
Rana Pratap Singh <134826533+im-rana-pratap@users.noreply.github.com>
2025-01-24 20:09:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new typed(".text",{
+    String:["Frontend Developer, Backend Developer,App Developer,"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+
+
+
+
+
+
+
